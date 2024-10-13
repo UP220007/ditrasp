f1a702fc2909c3e27544bdd772d48d4d53a5df15
Juan Eduardo Rangel Macias <up220007@alumnos.upa.edu.mx>
2024-10-13 14:21:21 -0600

    internto 2 compativilidad y visual

--- reconocimiento_facial.py
+++ reconocimiento_facial.py
@@ -1,4 +1,5 @@
 import cv2
+import matplotlib.pyplot as plt
 import os
 
 # Cargar el modelo previamente entrenado
@@ -10,10 +11,8 @@ opencv_version = cv2.__version__
 
 # Determinar la ruta del Haar Cascade según la versión de OpenCV
 if int(opencv_version.split('.')[0]) >= 4 and hasattr(cv2, 'data'):
-    # OpenCV 4.6.0 o superior
     haar_cascade_path = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 else:
-    # Versiones anteriores de OpenCV
     haar_cascade_path = '/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
 
 # Verificar si el archivo Haar Cascade existe
@@ -51,7 +50,7 @@ while True:
         label, confidence = face_recognizer.predict(rostro)
 
         # Mostrar el nombre de la persona si se reconoce con confianza suficiente
-        if confidence < 70:  # El valor de confianza puede ajustarse según la precisión
+        if confidence < 70:
             print(f"Reconocido: {people[label]} con confianza: {confidence:.2f}")
             cv2.putText(frame, f"{people[label]} ({confidence:.2f})", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
         else:
@@ -60,8 +59,11 @@ while True:
 
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    # Mostrar el video con las detecciones
-    cv2.imshow("Reconocimiento Facial", frame)
+    # Mostrar el video con las detecciones usando Matplotlib
+    plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    plt.axis('off')  # Ocultar ejes
+    plt.pause(0.001)  # Breve pausa para permitir la visualización
+    plt.clf()  # Limpiar la figura para la siguiente iteración
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
