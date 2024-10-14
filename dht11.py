6edae9c18198a3c6fb35892938e20b8fa0ee81d1
Juan Eduardo Rangel Macias <up220007@alumnos.upa.edu.mx>
2024-10-13 18:17:36 -0600

    dht11

---
+++ dht11.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT11
+pin_sensor = 7
+
+
+GPIO.setmode(GPIO.BOARD)
+
+# Este es el comando de la librería para leer del sensor
+humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin_sensor)
+
+# Si hay valores del sensor (por que aveces puede no mandar nada si se lee muy seguido) los imprime en pantalla para saber a qué temperatura estamos
+if humedad is not None and temperatura is not None:
+    print('Temp={0:0.1f}*C  humedad={1:0.1f}%'.format(temperatura, humedad))
+else:
+    print('Hubo un fallo al leer del sensor. Intentalo de nuevo!')
+
+GPIO.cleanup()
