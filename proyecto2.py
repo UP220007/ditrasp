a662519c71a868531137bd6bc4362274e6cc59c5
Juan Eduardo Rangel Macias <up220007@alumnos.upa.edu.mx>
2024-10-14 03:44:16 -0600

    correccion final 2

--- proyecto2.py
+++ proyecto2.py
@@ -149,12 +149,13 @@ def reconocimiento_facial(frame):
         else:
             print(f"Desconocido con confianza: {confidence:.2f}")
             GPIO.output(GPIO_LED_RED, GPIO.HIGH)
+            print("buzzer activo")
             GPIO.output(GPIO_BUZZER, GPIO.HIGH)
-            time.sleep(0.5)
+            time.sleep(1)
             GPIO.output(GPIO_BUZZER, GPIO.LOW)
-            time.sleep(0.5)
+            time.sleep(1)
             GPIO.output(GPIO_BUZZER, GPIO.HIGH)
-            time.sleep(0.5)
+            time.sleep(1)
             GPIO.output(GPIO_BUZZER, GPIO.LOW)
             
             return False  # Usuario desconocido
